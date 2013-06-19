a6f96f7866292a7d4b6d838993d9d9fabfee627c
tei219 <tei219@gmail.com>
2013-06-19 19:12:44 +0900

    Create serviceinfo.bat

---
+++ serviceinfo/serviceinfo.bat
@@ -0,0 +1,22 @@
+@echo off
+
+if not exist bin\psexec.exe (
+  echo [ERR ] missing 'bin\psexec.exe'
+	echo [INFO] search on google! http://www.google.co.jp/search?q=psexec.exe
+	pause
+	goto :EOB
+)
+
+if not exist bin\1st_launch (
+	bin\psexec.exe /? >nul
+	echo 1 > bin\1st_launch
+)
+
+if "%1"=="" (
+	echo "[ERR ] - missing target"
+) else (
+	bin\psservice.exe \\%1 query  > %1.txt
+)
+
+:EOB
+goto :EOF
