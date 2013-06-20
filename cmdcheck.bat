70a9faea4722fc119192f7fb56c4b56650346379
OpenShift guest <tei219@gmail.com>
2013-06-20 04:54:57 -0400

    Thu Jun 20 04:54:56 EDT 2013

--- cmdcheck.bat
+++ cmdcheck.bat
@@ -3,14 +3,16 @@ setlocal
 pushd %CD%
 cd /d %~d0%~p0
 
+set cmdsspec=conf\cmds_spec
+
 if exist cmd_missing ( del cmd_missing )
-if exist conf\cmds_spec ( del conf\cmds_spec )
+if exist %cmdsspec% ( del %cmdsspec% )
 
 if exist conf\required_commands.txt (
     for /f "usebackq tokens=*" %%c in (`type conf\required_commands.txt`) do (
 		for %%b in (%%c) do (
 			if exist %%~$path:b (
-				echo %%b	%%~$path:b >> conf\cmds_spec
+				echo %%b	%%~$path:b >> %cmdsspec%
 			) else (
 				echo [ERR ] missing '%%c'
 				echo [INFO] search on google! "http://www.google.co.jp/search?q=%%c"
@@ -28,8 +30,8 @@ if exist cmd_missing (
 	del conf\cmds_spec
 )
 
-if exist conf\cmds_spec (
-	echo [INFO] created 'conf\cmds_spec' 
+if exist %cmdsspec% (
+	echo [INFO] created '%cmdsspec%' 
 ) else (
 	echo [ERR ] command check failed.
 )
