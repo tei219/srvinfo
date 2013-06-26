0d015a78615ec4c1c3fbfb684a83283b110a3097
OpenShift guest <tei219@gmail.com>
2013-06-26 15:14:37 -0400

    Wed Jun 26 15:14:37 EDT 2013

---
+++ hostsinfo/hostsinfo.bat
@@ -0,0 +1,32 @@
+@echo off
+
+set cmdsspec=..\conf\cmds_spec
+set reqcmd1=psexec.exe
+set cmd1=
+
+if not exist %cmdsspec% (
+    echo [ERR ] missing '%cmdsspec%'
+	echo [INFO] do 'cmdcheck.bat' first.
+	goto :EOB
+) else (
+	for /f "usebackq tokens=1,2 delims=	" %%x in (%cmdsspec%) do (
+		if "%%x"=="%reqcmd1%" ( set cmd1=%%y )
+	)
+)
+
+if "%cmd1%"=="" (
+	echo [ERR ] missing commands '%reqcmd1%' on '%cmdsspec%'
+	pause
+	goto :EOB
+)
+
+:do_batch
+if "%1"=="" (
+	echo "[ERR ] missing target"
+) else (
+	echo [INFO] "%cmd1% \\%1 cmd /c type %SYSTEMROOT%\system32\drivers\etc\hosts > %1.txt"
+	%cmd1% \\%1 cmd /c type %SYSTEMROOT%\system32\drivers\etc\hosts > %1.txt
+)
+
+:EOB
+goto :EOF
